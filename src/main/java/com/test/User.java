2defdea3af3407c676a728103e38f6c61685cf38
Bittu Thakur <bittu.thkr@gmail.com>
2018-04-15 16:17:47 +0530

    Adds getter setter

--- src/main/java/com/test/User.java
+++ src/main/java/com/test/User.java
@@ -1,5 +1,16 @@
 package com.test;
 
 public class User {
+	private String userName;
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	
+	
 
 }
