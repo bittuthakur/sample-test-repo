976140872a8886ae1843d30298f1fe4000f77337
Bittu Thakur <bittu.thkr@gmail.com>
2018-04-15 16:27:44 +0530

    Adds age property

--- src/main/java/com/test/User.java
+++ src/main/java/com/test/User.java
@@ -2,4 +2,15 @@ package com.test;
 
 public class User {
 
+	private Integer age;
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+	
+	
 }
