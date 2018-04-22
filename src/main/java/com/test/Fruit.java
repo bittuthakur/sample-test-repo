d4984f6eb33a1c77e28fd5b9fd0e2f9751a6c89c
Bittu Thakur <bittu.thkr@gmail.com>
2018-04-23 01:05:52 +0530

    Adds setter getter

--- src/main/java/com/test/Fruit.java
+++ src/main/java/com/test/Fruit.java
@@ -3,4 +3,14 @@ package com.test;
 public class Fruit {
 	private String fruitName;
 
+	public String getFruitName() {
+		return fruitName;
+	}
+
+	public void setFruitName(String fruitName) {
+		this.fruitName = fruitName;
+	}
+	
+	
+
 }
